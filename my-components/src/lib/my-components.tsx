90c76170f2f3b4d4bdfcd572beac0b4411756b51
Guilhem Brouat <guilhem.brouat@heetch.com>
2024-03-11 11:16:49 +0100

    generate lib

---
+++ my-components/src/lib/my-components.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface MyComponentsProps {}
+
+export function MyComponents(props: MyComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to MyComponents!</h1>
+    </div>
+  );
+}
+
+export default MyComponents;
